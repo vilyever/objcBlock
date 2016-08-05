182d1288313d17859a6626bdc95c514839ccfd01
vilyever <250450035@163.com>
2016-08-05 09:52:36 +0800

    Initial Commit

---
+++ objcBlock/objcBlock.h
@@ -0,0 +1,19 @@
+//
+//  objcBlock.h
+//  objcBlock
+//
+//  Created by FTET on 16/8/5.
+//  Copyright © 2016年 vilyever. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objcBlock.
+FOUNDATION_EXPORT double objcBlockVersionNumber;
+
+//! Project version string for objcBlock.
+FOUNDATION_EXPORT const unsigned char objcBlockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objcBlock/PublicHeader.h>
+
+
