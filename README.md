34db2562c8571f98e3776fd1eef796cec0529946
vilyever <250450035@163.com>
2016-08-05 10:04:06 +0800

    release 1.0.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# objcBlock
+objcBlock
+
+## Import
+### [CocoaPods](http://cocoapods.org)
+use_frameworks!
+</br>
+pod 'objcBlock'
+
+### [Carthage](https://github.com/Carthage/Carthage)
+github "vilyever/objcBlock" "master"
+
+## Usage
+
+## License
+
+[MIT license](LICENSE)
--- objcBlock/objcBlock.h
+++ objcBlock/objcBlock.h
@@ -16,4 +16,40 @@ FOUNDATION_EXPORT const unsigned char objcBlockVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <objcBlock/PublicHeader.h>
 
+/**
+ *  Declare Block
+ *  
+ *  Local Var:
+ *      returnType (^blockName)(paramsType) = ^returnType(params){};
+ *      void(^action)(void) = ^void(){};
+ *
+ *
+ *  Property:
+ *      @property (nonatomic, copy) returnType (^blockName)(paramsType);
+ *      @property (nonatomic, copy) void (^action)(void);
+ *
+ *
+ *  Declare Method Param:
+ *      - (void)doAction:(returnType (^)(paramsType))blockName;
+ *      - (void)doAction:(void (^)(void))action;
+ *
+ *
+ *  Call Method Argument:
+ *      [self doAction:^void(paramsType){}];
+ *      [self doAction:^void(){}];
+ *
+ *
+ *  typedef:
+ *      typedef returnType (^TypedefName)(paramsType);
+ *      TypedefName blockName = ^returnType (paramsType){};
+ *
+ */
 
+#pragma mark weak self for block
+#if !VDWeakifySelf
+#define VDWeakifySelf \
+__weak __typeof(&*self)vd_weak_object = self
+
+#define VDStrongifySelf \
+__strong __typeof(&*vd_weak_object)self = vd_weak_object
+#endif
